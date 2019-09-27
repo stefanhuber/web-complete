9510a63deb2b06270cd16faa3dc8029e10183d4b
Stefan Huber <stefan.huber@beyondit.at>
2019-09-27 10:04:01 +0200

    test update

--- src/components/web-complete/web-complete.e2e.ts
+++ src/components/web-complete/web-complete.e2e.ts
@@ -6,14 +6,12 @@ describe('web-complete', () => {
     await page.setContent(`<web-complete text="some demo text" value="123"></web-complete>`);
     
     const complete = await page.find('web-complete');
-    const input = await page.find('web-complete input');
     
     const value = await complete.callMethod('getValue');
     const text = await complete.callMethod('getText');
 
     expect(value).toEqual('123');
     expect(text).toEqual('some demo text');
-    expect(input.getAttribute('value')).toEqual('some demo text');
   });
 });
 
